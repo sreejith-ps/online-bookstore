0e0f991ccb0e7525c7c7e4573268a99d555b8a92
Sreejith PS <mail2sreejithps@gmail.com>
2021-11-13 11:47:19 +0400

    Added service-registry

---
+++ book-service-registry/src/test/java/com/sps/bookstore/BookServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sps.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
